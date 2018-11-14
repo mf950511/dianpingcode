71ac1ae48f224598225719d3fe1b57d14dbcf9a1
mf950511 <1099693871@qq.com>
2018-11-14 15:31:58 +0800

    first commit

---
+++ src/containers/index/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Banner from './subpage/banner'
+import './index.less'
+class Index extends Component {
+  render(){
+    return (
+      <div>
+        <p>哈哈哈</p>
+        <Banner/>
+      </div>
+    )
+  }
+}
+export default Index
